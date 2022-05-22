d01cd4ed097d9e2a9bcfc0d97ffcd5926a5b422b
Kevin Whetten <krwhetten@gmail.com>
2022-05-22 01:41:21 -0600

    Added story component.

---
+++ TravellerCharacterCreator/ClientApp/src/app/controls/story/story.component.ts
@@ -0,0 +1,27 @@
+﻿import {Component, OnInit} from '@angular/core';
+import {LoggingService} from "../../services/metadata-services/logging.service";
+
+@Component({
+  selector: 'app-story',
+  templateUrl: './story.component.html',
+  styleUrls: ['./story.component.css']
+})
+export class StoryComponent implements OnInit {
+  story: string = '';
+  submitted: boolean = false;
+  disabled: string = '';
+
+  constructor(private _loggingService: LoggingService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  submitStory() {
+    if (this.story) {
+      this._loggingService.addLog(this.story);
+      this.submitted = true;
+      this.disabled = 'disabled';
+    }
+  }
+}
